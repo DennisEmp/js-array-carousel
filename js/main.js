bc4e9c6771c308906ab00e90475ebac1a925a746
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-11 18:20:16 +0100

    array creata, btn aggiunto e dichiarato da fare: addeventlist click al btn con active che cambia classe

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-let immagini [];
+// creazione array con img
 
-// creazione array
+const imgArr  = [`img/01.webp`, `img/02.webp`,`img/03.webp`,`img/04.webp`,`img/05.webp`,];
+
+console.log(imgArr);
+
+const output = document.getElementById("output");
+
+for (let i = 0; i < imgArr.length; i++) {
+    output.innerHTML += `<img src="${imgArr[i]}" alt=">`
+};
+
+
+// dichiaro btn right
+const buttonRight = document.querySelector("btn btn-light");
+
+
+
+
+// add event list al click con active che cambia classe
