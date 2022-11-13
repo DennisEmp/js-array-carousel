eaa28fefb658ba7d65417b910845828ac72f949a
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-13 20:24:09 +0100

    solo bttn right

--- js/main.js
+++ js/main.js
@@ -2,19 +2,30 @@
 
 const imgArr  = [`img/01.webp`, `img/02.webp`,`img/03.webp`,`img/04.webp`,`img/05.webp`,];
 
-console.log(imgArr);
+const slide = document.getElementById("box1");
 
-const output = document.getElementById("output");
+let active = 0;
 
 for (let i = 0; i < imgArr.length; i++) {
-    output.innerHTML += `<img src="${imgArr[i]}" alt=">`
+    slide.innerHTML += `<img src="${imgArr[i]}" id="img-${i}" alt="" class="none">`
 };
 
+let box = document.querySelector(".box").classList.add("block");
+let imgActive = document.getElementById("img-" + active);
+imgActive.classList.add("block");
+let btnRight = document.querySelector(".btn-right");
 
-// dichiaro btn right
-const buttonRight = document.querySelector("btn btn-light");
+btnRight.addEventListener("click" , function(){
+
+    active++
+    if (active > ((imgArr.length) - 1)){
+        active = 0}
+        imgActive.classList.remove("block")
+        imgActive = document.getElementById("img-" + active);
+        imgActive.classList.add("block")
+});
+
+console.log(btnRight)
 
 
 
-
-// add event list al click con active che cambia classe
